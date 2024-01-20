cdfaf7c7335308ede22d475dafdef20c83e73d41
kanokoga <kanokoga@gmail.com>
2024-01-20 23:05:40 +0900

    update:

---
+++ CameraMovers/CameraRotater.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NipaFriends.CameraMovers
+{
+    public class CameraRotater : MonoBehaviour
+    {
+        [SerializeField] private Transform lookTarget;
+        [SerializeField] private Transform childInLookTarget;
+        [SerializeField] private Transform camera;
+        public float rotationSpeed = 1f;
+        public float zoomSpeed = 1f;
+
+        private void Awake()
+        {
+            this.camera.LookAt(this.lookTarget);
+        }
+
+        private void Update()
+        {
+            if(Input.GetMouseButton(1) == true)
+            {
+                var mousePositionDeltaNormalized = Input.mousePositionDelta;
+                mousePositionDeltaNormalized.x /= Screen.width;
+                mousePositionDeltaNormalized.y /= Screen.height;
+                this.lookTarget.Rotate(0f, mousePositionDeltaNormalized.x * this.rotationSpeed, 0f);
+                this.childInLookTarget.Rotate(-mousePositionDeltaNormalized.y * this.rotationSpeed, 0f, 0f);
+            }
+
+            var mouseScrollDelta = Input.mouseScrollDelta;
+            if(mouseScrollDelta.y != 0f)
+            {
+                var scrollDelta = mouseScrollDelta.y > 0f ? 1f : -1f;
+                this.camera.Translate(0f, 0f, scrollDelta * Time.deltaTime * this.zoomSpeed);
+            }
+        }
+    }
+}
