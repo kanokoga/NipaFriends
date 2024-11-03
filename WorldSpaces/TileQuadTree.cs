5f5d8b25a71fa834effbca29ebedd785f2eb6139
kanokoga <kanokoga@gmail.com>
2024-11-04 02:05:30 +0900

    update:

--- WorldSpaces/TileQuadTree.cs
+++ WorldSpaces/TileQuadTree.cs
@@ -18,77 +18,56 @@ namespace NipaFriends.WorldSpaces
         public void SetBool(Vector2Int pos, bool value)
         {
             this.grid[pos.x, pos.y] = value;
-            this.quadTree = new QuadTree(0, 0, this.grid.GetLength(0), this.grid.GetLength(1), this.grid,
-                true);
-            this.quadTrees = this.quadTree.GetLargestObstacleFreeRegions();
-            Debug.Log(this.quadTrees.Count);
+            this.quadTrees = new List<QuadTree>();
+            this.AddQT(0, 0, this.grid.GetLength(0), this.grid.GetLength(1), this.grid, this.quadTrees);
         }
-        
-        public class QuadTree
+
+        private bool ContainsObstacle(int x, int y, int width, int height, bool[,] grid)
         {
-            public bool HasObstacle { get; private set; }
+            for(var i = x; i < x + width; i++)
+            {
+                for(var j = y; j < y + height; j++)
+                {
+                    if(grid[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void AddQT(int x, int y, int width, int height, bool[,] grid, List<QuadTree> regions)
+        {
+            if(this.ContainsObstacle(x, y, width, height, grid) == true && (width > 1 || height > 1))
+            {
+                var halfWidth = width / 2;
+                var halfHeight = height / 2;
+                this.AddQT(x, y, halfWidth, halfHeight, grid, regions);
+                this.AddQT(x + halfWidth, y, width - halfWidth, halfHeight, grid, regions);
+                this.AddQT(x, y + halfHeight, halfWidth, height - halfHeight, grid, regions);
+                this.AddQT(x + halfWidth, y + halfHeight, width - halfWidth, height - halfHeight, grid, regions);
+            }
+            else
+            {
+                regions.Add(new QuadTree(x, y, width, height));
+            }
+        }
+
+        public struct QuadTree
+        {
             public int X { get; private set; }
             public int Y { get; private set; }
             public int Width { get; private set; }
             public int Height { get; private set; }
 
-            public QuadTree TopLeft, TopRight, BottomLeft, BottomRight;
-
-            public QuadTree(int x, int y, int width, int height, bool[,] grid, bool isRoot = false)
+            public QuadTree(int x, int y, int width, int height)
             {
                 this.X = x;
                 this.Y = y;
                 this.Width = width;
                 this.Height = height;
-                this.HasObstacle = this.ContainsObstacle(x, y, width, height, grid);
-
-                if(this.HasObstacle == true && (width > 1 || height > 1))
-                {
-                    int halfWidth = width / 2;
-                    int halfHeight = height / 2;
-
-                    this.TopLeft = new QuadTree(x, y, halfWidth, halfHeight, grid);
-                    this.TopRight = new QuadTree(x + halfWidth, y, width - halfWidth, halfHeight, grid);
-                    this.BottomLeft = new QuadTree(x, y + halfHeight, halfWidth, height - halfHeight, grid);
-                    this.BottomRight = new QuadTree(x + halfWidth, y + halfHeight, width - halfWidth,
-                        height - halfHeight, grid);
-                }
-            }
-
-            private bool ContainsObstacle(int x, int y, int width, int height, bool[,] grid)
-            {
-                for(int i = x; i < x + width; i++)
-                {
-                    for(int j = y; j < y + height; j++)
-                    {
-                        if(grid[i, j])
-                        {
-                            return true;
-                        }
-                    }
-                }
-
-                return false;
-            }
-
-            public List<QuadTree> GetLargestObstacleFreeRegions()
-            {
-                List<QuadTree> regions = new List<QuadTree>();
-
-
-                if(this.TopLeft == null)
-                {
-                    regions.Add(this);
-                }
-                else
-                {
-                    regions.AddRange(this.TopLeft.GetLargestObstacleFreeRegions());
-                    regions.AddRange(this.TopRight.GetLargestObstacleFreeRegions());
-                    regions.AddRange(this.BottomLeft.GetLargestObstacleFreeRegions());
-                    regions.AddRange(this.BottomRight.GetLargestObstacleFreeRegions());
-                }
-
-                return regions;
             }
         }
     }
